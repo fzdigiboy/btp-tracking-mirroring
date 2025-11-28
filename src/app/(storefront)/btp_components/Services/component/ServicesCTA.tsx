7058db2d05acb8c26e1d551571ac890e549d508b
BOUANGAElsira <elsirema3@gmail.com>
2025-11-28 12:52:16 +0000

    Les 4 blocks presque identiques

--- src/app/(storefront)/btp_components/Services/component/ServicesCTA.tsx
+++ src/app/(storefront)/btp_components/Services/component/ServicesCTA.tsx
@@ -1,14 +1,30 @@
-export default function ServicesCTA() {
+export default function ServicesCTA(
+    {
+        title,
+        description,
+        titleColor,
+        descriptionColor,
+        backgroundColor,
+        sectionBgColor,
+        isFullWidth,
+        button,
+        // haveButton
+    }: any
+) {
     return (
-        <section className="w-full bg-primary text-white rounded-xl p-8 sm:p-12 my-12 text-center">
-            <div className="flex flex-col items-center gap-6">
-                <h2 className="text-3xl font-bold">Ready to build your dream in Togo?</h2>
-                <p className="max-w-2xl">
-                    Let's turn your vision into reality. Contact our team today for a free, no-obligation consultation to discuss your project requirements.
-                </p>
-                <button className="flex min-w-[84px] cursor-pointer items-center justify-center overflow-hidden rounded-lg h-12 px-6 bg-secondary text-primary text-base font-bold shadow-lg hover:opacity-90 transition-opacity">
-                    <span className="truncate">Tell Us About Your Project</span>
-                </button>
+        <section className={`py-16 sm:py-24 ${isFullWidth === 'Yes' ? '' : 'container mx-auto px-4'}`} style={{ backgroundColor: sectionBgColor }}>
+            <div className={`rounded-xl p-8 sm:p-12 text-center`} style={{ backgroundColor: backgroundColor }}>
+                <div className="flex flex-col items-center gap-6">
+                    <h2 className="text-3xl font-bold" style={{ color: titleColor }}>{title}</h2>
+                    <p className="max-w-2xl" style={{ color: descriptionColor }}>
+                        {description}
+                    </p>
+                    {/* {haveButton && ( */}
+                        <button className={`flex min-w-[84px] cursor-pointer items-center justify-center overflow-hidden rounded-lg h-12 px-6 text-base font-bold shadow-lg hover:opacity-90 transition-opacity`} style={{ backgroundColor: button?.color, color: button?.textColor }}>
+                            <span className="truncate">{button?.text}</span>
+                        </button>
+                    {/* )} */}
+                </div>
             </div>
         </section>
     );
