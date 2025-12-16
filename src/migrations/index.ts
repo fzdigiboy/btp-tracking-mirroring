fda54517c9bcbda23a9b2c5619110c56d674e74a
Aklesso  GNANDA <aklesso.digitaleurs@gmail.com>
2025-12-16 15:55:19 +0000

    review upload image on production

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20251215_130924 from './20251215_130924';
+import * as migration_20251216_104531 from './20251216_104531';
 
 export const migrations = [
   {
     up: migration_20251215_130924.up,
     down: migration_20251215_130924.down,
-    name: '20251215_130924'
+    name: '20251215_130924',
+  },
+  {
+    up: migration_20251216_104531.up,
+    down: migration_20251216_104531.down,
+    name: '20251216_104531'
   },
 ];
